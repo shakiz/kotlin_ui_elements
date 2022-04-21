d6639bf44e9cefe220276f49d2923d7c485698b5
shakiz <activities335@gmail.com>
2022-04-21 23:25:59 +0600

    Initial commit

---
+++ app/src/main/java/com/practice/uielements/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.practice.uielements
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.practice.uielements.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.showAlertDialog.setOnClickListener { showAlert() }
+
+        binding.showBasicAlert.setOnClickListener { showBasicAlert() }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Logout Confirmation")
+        builder.setMessage("Do you want to logout from the app?")
+        builder.setPositiveButton("Yes") { dialog, which ->
+            Toast.makeText(
+                applicationContext,
+                "Logged out", Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        builder.setNegativeButton("No") { dialog, which ->
+            Toast.makeText(
+                applicationContext,
+                "Stay with us", Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        builder.setNeutralButton("Maybe") { dialog,
+                                            which ->
+            Toast.makeText(
+                applicationContext,
+                "Not sure", Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        builder.show();
+    }
+
+    private fun showBasicAlert(){
+        val builder = AlertDialog.Builder(this)
+        with(builder){
+            setTitle("Basic Alert")
+            setMessage("Basic alert dialog message")
+            setPositiveButton("Ok"){dialog, which -> Toast.makeText(applicationContext,
+                "Ok Clicked", Toast.LENGTH_SHORT).show()}
+            setNegativeButton("Cancel"){dialog, which -> Toast.makeText(applicationContext,
+                "Cancel Clicked", Toast.LENGTH_SHORT).show()}
+            show()
+        }
+    }
+}
